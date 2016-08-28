7926aba567acb9906ef0be30385f9a02014f68f5
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-08-28 15:46:51 +0200

    99 bottles exercise ported to C#

---
+++ 99BottlesOfOOP/Bottles.cs
@@ -0,0 +1,20 @@
+﻿namespace _99BottlesOfOOP
+{
+    public class Bottles
+    {
+        public string Verse(int number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Verses(int first, int last)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Song()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
