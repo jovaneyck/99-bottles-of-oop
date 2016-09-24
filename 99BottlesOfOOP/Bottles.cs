299d74a25e0bc13e4bc7a0efdaacca3b994d6103
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-09-24 12:16:47 +0200

    2 and 1 by explicit hardcoding. no rule of three just yet (or if you squint just maybe with general case

--- 99BottlesOfOOP/Bottles.cs
+++ 99BottlesOfOOP/Bottles.cs
@@ -3,7 +3,15 @@
     public class Bottles
     {
         public string Verse(int number)
-        {   
+        {
+            if (number == 1)
+                return @"1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.";
+
+            if (number == 2)
+                return @"2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.";
+
             return $@"{number} bottles of beer on the wall, {number} bottles of beer.
 Take one down and pass it around, {number - 1} bottles of beer on the wall.";
         }
