bd3ece1126c067eaadf41fa89ad30499d03517c4
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-09-24 12:26:43 +0200

    cleaned up verse logic a bit. Still no hardcore DRYing

--- 99BottlesOfOOP/Bottles.cs
+++ 99BottlesOfOOP/Bottles.cs
@@ -7,20 +7,17 @@ namespace _99BottlesOfOOP
     {
         public string Verse(int number)
         {
-            if (number == 0)
-                return @"No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.";
- 
-            if (number == 1)
-                return @"1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.";
+            switch (number)
+            {
+                case 0:
+                    return "No more bottles of beer on the wall, no more bottles of beer.\r\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+                case 1:
+                    return "1 bottle of beer on the wall, 1 bottle of beer.\r\nTake it down and pass it around, no more bottles of beer on the wall.";
+                case 2:
+                    return "2 bottles of beer on the wall, 2 bottles of beer.\r\nTake one down and pass it around, 1 bottle of beer on the wall.";
+            }
 
-            if (number == 2)
-                return @"2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.";
-
-            return $@"{number} bottles of beer on the wall, {number} bottles of beer.
-Take one down and pass it around, {number - 1} bottles of beer on the wall.";
+            return $"{number} bottles of beer on the wall, {number} bottles of beer.\r\nTake one down and pass it around, {number - 1} bottles of beer on the wall.";
         }
 
         public string Verses(int first, int last)
