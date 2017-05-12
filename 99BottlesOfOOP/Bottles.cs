a8d1b4469bd7cf66e0a94890a8fc283998bd4e09
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-12 17:24:02 +0200

    making default case similar to 0 case

--- 99BottlesOfOOP/Bottles.cs
+++ 99BottlesOfOOP/Bottles.cs
@@ -26,7 +26,7 @@ namespace _99BottlesOfOOP
             switch (number)
             {
                 case 0:
-                    return $"{Quantity(number).Capitalize()} {Container(number)} of beer on the wall, no more bottles of beer.\r\nGo to the store and buy some more, 99 bottles of beer on the wall.";
+                    return $"{Quantity(number).Capitalize()} {Container(number)} of beer on the wall, {Quantity(number)} bottles of beer.\r\nGo to the store and buy some more, 99 bottles of beer on the wall.";
                 default:
                     return $"{Quantity(number).Capitalize()} {Container(number)} of beer on the wall, {Quantity(number)} {Container(number)} of beer.\r\nTake {Pronoun(number)} down and pass it around, {Quantity(number - 1)} {Container(number - 1)} of beer on the wall.";
             }
