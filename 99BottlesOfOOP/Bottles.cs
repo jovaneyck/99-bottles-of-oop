cd9cad43340529e16ba52df55eb5a4ddfeb1f36f
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-12 13:28:54 +0200

    made verse 2 and N identical so only a single difference remains

--- 99BottlesOfOOP/Bottles.cs
+++ 99BottlesOfOOP/Bottles.cs
@@ -30,7 +30,7 @@ namespace _99BottlesOfOOP
                 case 1:
                     return "1 bottle of beer on the wall, 1 bottle of beer.\r\nTake it down and pass it around, no more bottles of beer on the wall.";
                 case 2:
-                    return "2 bottles of beer on the wall, 2 bottles of beer.\r\nTake one down and pass it around, 1 bottle of beer on the wall.";
+                    return $"{number} bottles of beer on the wall, {number} bottles of beer.\r\nTake one down and pass it around, {number - 1} bottle of beer on the wall.";
                 default:
                     return $"{number} bottles of beer on the wall, {number} bottles of beer.\r\nTake one down and pass it around, {number - 1} bottles of beer on the wall.";
             }
