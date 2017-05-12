6b6484e5455c7afdd842a268298e0a278f40f5c9
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-12 15:54:34 +0200

    making the 1 and default cases more similar: no more hardcoded numbers

--- 99BottlesOfOOP/Bottles.cs
+++ 99BottlesOfOOP/Bottles.cs
@@ -28,7 +28,7 @@ namespace _99BottlesOfOOP
                 case 0:
                     return "No more bottles of beer on the wall, no more bottles of beer.\r\nGo to the store and buy some more, 99 bottles of beer on the wall.";
                 case 1:
-                    return "1 bottle of beer on the wall, 1 bottle of beer.\r\nTake it down and pass it around, no more bottles of beer on the wall.";
+                    return $"{number} bottle of beer on the wall, {number} bottle of beer.\r\nTake it down and pass it around, no more bottles of beer on the wall.";
                 default:
                     return $"{number} bottles of beer on the wall, {number} bottles of beer.\r\nTake one down and pass it around, {number - 1} {Container(number - 1)} of beer on the wall.";
             }
