fbe1f4560275d650c7dd85afc550e6fbd6b7ec8c
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-12 17:19:42 +0200

    0 case: use quantity and capitalize

---
+++ 99BottlesOfOOP/CapitalizationTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace _99BottlesOfOOP
+{
+    public class CapitalizationTests
+    {
+        [Theory]
+        [InlineData("","")]
+        [InlineData(null,null)]
+        [InlineData("A","A")]
+        [InlineData("a","A")]
+        [InlineData("ab","Ab")]
+        public void CapitalizesCorrectly(string input, string expected)
+        {
+            Assert.Equal(expected, input.Capitalize());
+        }        
+    }
+}
---
+++ 99BottlesOfOOP/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace _99BottlesOfOOP
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            var first = s.First().ToString();
+            var rest = new string(s.Skip(1).ToArray());
+
+            return first.ToUpper() + rest;
+        }
+    }
+}
