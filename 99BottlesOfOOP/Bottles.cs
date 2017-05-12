ea0f03c568a32bbc851d546b2013913cf2fe31c7
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-12 13:54:31 +0200

    remove the now-obsolete verse 2 logic

--- 99BottlesOfOOP/Bottles.cs
+++ 99BottlesOfOOP/Bottles.cs
@@ -29,8 +29,6 @@ namespace _99BottlesOfOOP
                     return "No more bottles of beer on the wall, no more bottles of beer.\r\nGo to the store and buy some more, 99 bottles of beer on the wall.";
                 case 1:
                     return "1 bottle of beer on the wall, 1 bottle of beer.\r\nTake it down and pass it around, no more bottles of beer on the wall.";
-                case 2:
-                    return $"{number} bottles of beer on the wall, {number} bottles of beer.\r\nTake one down and pass it around, {number - 1} {Container(number - 1)} of beer on the wall.";
                 default:
                     return $"{number} bottles of beer on the wall, {number} bottles of beer.\r\nTake one down and pass it around, {number - 1} {Container(number - 1)} of beer on the wall.";
             }
