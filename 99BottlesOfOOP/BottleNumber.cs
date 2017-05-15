0e33d99e5d6095bab26c71f9d967392da11d0745
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-15 17:24:20 +0200

    started working on the primitive obsession smell: copied bottle number methods to BottleNumber

--- 99BottlesOfOOP/BottleNumber.cs
+++ 99BottlesOfOOP/BottleNumber.cs
@@ -2,6 +2,51 @@ namespace _99BottlesOfOOP
 {
     public class BottleNumber
     {
-        
+        public string Container(int number)
+        {
+            if (number == 1)
+            {
+                return "bottle";
+            }
+
+            return "bottles";
+        }
+
+        public string Pronoun(int number)
+        {
+            if (number == 1)
+            {
+                return "it";
+            }
+            return "one";
+        }
+
+        public string Quantity(int number)
+        {
+            if (number == 0)
+            {
+                return "no more";
+            }
+            return number.ToString();
+        }
+
+        public string Action(int number)
+        {
+            if (number == 0)
+            {
+                return "Go to the store and buy some more";
+            }
+            return $"Take {Pronoun(number)} down and pass it around";
+        }
+
+        public int Successor(int number)
+        {
+            if (number == 0)
+            {
+                return 99;
+            }
+
+            return number - 1;
+        }
     }
 }
