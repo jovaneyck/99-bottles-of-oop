677d29a72e8fb8517ae42405b5a02cd5f9d98a35
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-05-19 08:31:45 +0200

    Use BottleNumber6 in the factory method. Making progress

--- 99BottlesOfOOP/BottleNumber.cs
+++ 99BottlesOfOOP/BottleNumber.cs
@@ -17,6 +17,8 @@ namespace _99BottlesOfOOP
                     return new BottleNumber0(number);
                 case 1:
                     return new BottleNumber1(number);
+                case 6:
+                    return new BottleNumber6(number);
                 default:
                     return new BottleNumber(number);
             }
